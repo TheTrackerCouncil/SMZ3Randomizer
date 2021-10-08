b1ff3dd2bc07e790af28c12ffff593a1df3b0321
Laura Verdoes <1766841+Vivelin@users.noreply.github.com>
2021-10-08 18:15:20 +0200

    Removed some soundtrack values that didn't work

--- Randomizer.SMZ3/FileData/ALttPSoundtrack.cs
+++ Randomizer.SMZ3/FileData/ALttPSoundtrack.cs
@@ -3,24 +3,24 @@
     public enum ALttPSoundtrack : byte
     {
         None = 0,
-        Title = 1,
+        // Title = 1, // Silent in dungeons
         LightWorldOverworld = 2,
         Rain = 3,
         Bunny = 4,
         LightLostWoods = 5,
         OpeningStoryCredits = 6,
         KakarikoVillage = 7,
-        Mirror = 8,
+        // Mirror = 8, // Untested, but I doubt it'll loop
         DarkWorldOverworld = 9,
-        MasterSwordPedestal = 10,
+        // MasterSwordPedestal = 10, / Silent, again
         FileSelect = 11,
-        GuardSummonedInKakarikoVillage = 12,
+        // GuardSummonedInKakarikoVillage = 12, // Silent in dungeons
         DarkDeathMountain = 13,
         GuessingGame = 14,
         HyruleCastle = 16,
         LightWorldDungeon = 17,
         Cave1 = 18,
-        BossKillFanfare = 19,
+        // BossKillFanfare = 19, // You did it! Too bad it's quiet afterwards for the rest of the dungeon
         Sanctuary = 20,
         BossFight = 21,
         DarkWorldDungeon = 22,
@@ -30,11 +30,11 @@
         CrystalGet = 26,
         FairyFountain = 27,
         GanonsTheme = 28,
-        GanonAppears = 29,
+        // GanonAppears = 29, // Kinda funny, but only plays once and then goes silent
         GanonInPyramid = 30,
         GanonFight = 31,
-        TriforceRoom = 32,
-        TriumphantReturn = 33,
+        // TriforceRoom = 32, // Silent in dungeons
+        // TriumphantReturn = 33, // Kinda funny, but only plays once and then goes silent
         Credits = 34,
     }
 }
