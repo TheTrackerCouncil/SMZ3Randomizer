fb5199b94da688eb47c5cd83669c07fcb74b9606
Laura Verdoes <1766841+Vivelin@users.noreply.github.com>
2021-11-13 13:55:31 +0100

    Converted tests from Nunit to Xunit

--- tests/Randomizer.SMZ3.Tests/ParseTests.cs
+++ tests/Randomizer.SMZ3.Tests/ParseTests.cs
@@ -1,64 +1,69 @@
 ﻿using System;
 
-using NUnit.Framework;
 
 using Randomizer.SMZ3.Generation;
 
+using Xunit;
+
 namespace Randomizer.SMZ3.Tests
 {
-    [TestFixture]
     public class ParseTests
     {
-        [TestCase("ABCDEF", 0x00ABCDEF)]
-        [TestCase("7FFFFFFF", int.MaxValue)]
-        [TestCase("FFFFFFFF", -1)]
-        [TestCase("80000000", int.MinValue)]
-        [TestCase("0x0", 0)]
-        [TestCase("0x7FFFFFFF", int.MaxValue)]
+        [Theory]
+        [InlineData("ABCDEF", 0x00ABCDEF)]
+        [InlineData("7FFFFFFF", int.MaxValue)]
+        [InlineData("FFFFFFFF", -1)]
+        [InlineData("80000000", int.MinValue)]
+        [InlineData("0x0", 0)]
+        [InlineData("0x7FFFFFFF", int.MaxValue)]
         public void AsHexConvertsHexadecimalNumbers(string input, int expected)
         {
             var result = Parse.AsHex(input, out var value);
 
-            Assert.IsTrue(result);
-            Assert.AreEqual(expected, value);
+            Assert.True(result);
+            Assert.Equal(expected, value);
         }
 
-        [TestCase("ABCDEFG")]
-        [TestCase("0x0x0x0")]
+        [Theory]
+        [InlineData("ABCDEFG")]
+        [InlineData("0x0x0x0")]
         public void AsHexReturnsFalseIfInputIsNotValid(string input)
         {
             var result = Parse.AsHex(input, out _);
 
-            Assert.IsFalse(result);
+            Assert.False(result);
         }
 
-        [TestCase("0xFFFFFFFFFFFFFFFF")]
+        [Theory]
+        [InlineData("0xFFFFFFFFFFFFFFFF")]
         public void AsHexReturnsFalseIfInputOverflows(string input)
         {
             var result = Parse.AsHex(input, out _);
 
-            Assert.IsFalse(result);
+            Assert.False(result);
         }
 
-        [TestCase("0", 0)]
-        [TestCase("2147483647", int.MaxValue)]
-        [TestCase("-2147483648", int.MinValue)]
+        [Theory]
+        [InlineData("0", 0)]
+        [InlineData("2147483647", int.MaxValue)]
+        [InlineData("-2147483648", int.MinValue)]
         public void AsIntegerConvertsPlainIntegers(string input, int expected)
         {
             var result = Parse.AsInteger(input, out var value);
 
-            Assert.IsTrue(result);
-            Assert.AreEqual(expected, value);
+            Assert.True(result);
+            Assert.Equal(expected, value);
         }
 
-        [TestCase("1.234.567,89")] // Thousands separators and decimal separators aren't allowed
-        [TestCase("1,234,567.89")] // Thousands separators and decimal separators aren't allowed
-        [TestCase("0xFF")] // Not a decimal integer
+        [Theory]
+        [InlineData("1.234.567,89")] // Thousands separators and decimal separators aren't allowed
+        [InlineData("1,234,567.89")] // Thousands separators and decimal separators aren't allowed
+        [InlineData("0xFF")] // Not a decimal integer
         public void AsIntegerReturnsFalseInvalidInts(string input)
         {
             var result = Parse.AsInteger(input, out _);
 
-            Assert.IsFalse(result);
+            Assert.False(result);
         }
     }
 }
