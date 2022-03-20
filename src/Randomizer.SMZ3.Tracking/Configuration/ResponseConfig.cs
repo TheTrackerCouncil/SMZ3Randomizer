e2df6848a4740811d1e24512881390ec597dad94
Laura Verdoes <1766841+Vivelin@users.noreply.github.com>
2022-03-20 13:01:24 +0100

    Allow tracker to complain about getting interrupted while talking

--- src/Randomizer.SMZ3.Tracking/Configuration/ResponseConfig.cs
+++ src/Randomizer.SMZ3.Tracking/Configuration/ResponseConfig.cs
@@ -603,6 +603,11 @@ namespace Randomizer.SMZ3.Tracking.Configuration
             = new SchrodingersString("But you haven't defeated {0} yet.");
 
         /// <summary>
+        /// Gets the phrases to respond with when Tracker is being interrupted.
+        /// </summary>
+        public SchrodingersString? Interrupted { get; init; }
+
+        /// <summary>
         /// Gets a dictionary that contains the phrases to respond with when no
         /// voice commands have been issued after a certain period of time, as
         /// expressed in the dictionary keys.
