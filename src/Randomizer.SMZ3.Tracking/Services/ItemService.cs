dd216b3c6f204082dd915515a5e6044ff06ae300
Laura Verdoes <1766841+Vivelin@users.noreply.github.com>
2022-07-23 14:33:37 +0200

    Add documentation and extract ItemService interface

--- src/Randomizer.SMZ3.Tracking/Services/ItemService.cs
+++ src/Randomizer.SMZ3.Tracking/Services/ItemService.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using BunLabs;
 
 using Randomizer.Shared;
 using Randomizer.SMZ3.Tracking.Configuration;
@@ -15,10 +10,18 @@ namespace Randomizer.SMZ3.Tracking.Services
     /// <summary>
     /// Manages items and their tracking state.
     /// </summary>
-    public class ItemService
+    public class ItemService : IItemService
     {
-        private static Random s_random = new Random();
+        private static readonly Random s_random = new Random();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ItemService"/> class
+        /// with the specified dependencies.
+        /// </summary>
+        /// <param name="trackerConfig">
+        /// Specifies the configuration that contains the item data to be
+        /// managed.
+        /// </param>
         public ItemService(TrackerConfig trackerConfig)
         {
             Items = trackerConfig.Items;
@@ -29,46 +32,88 @@ namespace Randomizer.SMZ3.Tracking.Services
         /// </summary>
         protected IReadOnlyCollection<ItemData> Items { get; }
 
+        /// <summary>
+        /// Finds the item with the specified name.
+        /// </summary>
+        /// <param name="name">
+        /// The name of the item or item stage to find.
+        /// </param>
+        /// <returns>
+        /// An <see cref="ItemData"/> representing the item with the specified
+        /// name, or <see langword="null"/> if there is no item that has the
+        /// specified name.
+        /// </returns>
         public virtual ItemData? Find(string name)
             => Items.SingleOrDefault(x => x.Name.Contains(name, StringComparison.OrdinalIgnoreCase))
             ?? Items.SingleOrDefault(x => x.GetStage(name) != null);
 
+        /// <summary>
+        /// Finds an item with the specified item type.
+        /// </summary>
+        /// <param name="itemType">The type of item to find.</param>
+        /// <returns>
+        /// An <see cref="ItemData"/> representing the item. If there are
+        /// multiple configured items with the same type, this method returns
+        /// one at random. If there no configured items with the specified type,
+        /// this method returns <see langword="null"/>.
+        /// </returns>
         public virtual ItemData? Get(ItemType itemType)
             => Items.RandomOrDefault(x => x.InternalItemType == itemType, s_random);
 
-        public virtual bool IsTracked(ItemType itemType)
-            => Items.Any(x => x.InternalItemType == itemType && x.TrackingState > 0);
-
-        public virtual IEnumerable<ItemData> AllItems() // I really want to discourage this, but necessary for now
-            => Items;
-
-        public virtual IEnumerable<ItemData> TrackedItems()
-            => Items.Where(x => x.TrackingState > 0);
-
-        [Obsolete("Use LocationService to get items from location when available")]
-        public virtual ItemData? Get(Location location)
+        /// <summary>
+        /// Returns the item data of the item at the specified location.
+        /// </summary>
+        /// <param name="location">The location whose item to check.</param>
+        /// <returns>
+        /// An <see cref="ItemData"/> representing the type of item found at the
+        /// specified location, or <see langword="null"/> if the location has no
+        /// item or if the item at the location is not configured.
+        /// </returns>
+        public virtual ItemData? Get(Location location) // TODO: move this to IWorldService, whenever that becomes available
             => Get(location.Item.Type);
 
         /// <summary>
-        /// Returns a random name for the specified item including article,
-        /// e.g. "an E-Tank" or "the Book of Mudora".
+        /// Indicates whether an item of the specified type has been tracked.
+        /// </summary>
+        /// <param name="itemType">The type of item to check.</param>
+        /// <returns>
+        /// <see langword="true"/> if an item with the specified type has been
+        /// tracked at least once; otherwise, <see langword="false"/>.
+        /// </returns>
+        public virtual bool IsTracked(ItemType itemType)
+            => Items.Any(x => x.InternalItemType == itemType && x.TrackingState > 0);
+
+        /// <summary>
+        /// Enumerates all items that can be tracked.
+        /// </summary>
+        /// <returns>A collection of items.</returns>
+        public virtual IEnumerable<ItemData> AllItems() // I really want to discourage this, but necessary for now
+            => Items;
+
+        /// <summary>
+        /// Enumarates all currently tracked items.
+        /// </summary>
+        /// <returns>
+        /// A collection of items that have been tracked at least once.
+        /// </returns>
+        public virtual IEnumerable<ItemData> TrackedItems()
+            => Items.Where(x => x.TrackingState > 0);
+
+        /// <summary>
+        /// Returns a random name for the specified item including article, e.g.
+        /// "an E-Tank" or "the Book of Mudora".
         /// </summary>
         /// <param name="itemType">The type of item whose name to get.</param>
-        /// <returns>The name of the type of item, including "a", "an" or "the" if applicable.</returns>
+        /// <returns>
+        /// The name of the type of item, including "a", "an" or "the" if
+        /// applicable.
+        /// </returns>
         public virtual string GetName(ItemType itemType)
         {
             var item = Get(itemType);
             return item?.NameWithArticle ?? itemType.GetDescription();
         }
 
-        public virtual void Track(ItemData itemData/* ... */)
-        {
-            throw new NotImplementedException();
-        }
-
-        public virtual void Track(ItemType itemType/* ... */)
-        {
-            throw new NotImplementedException();
-        }
+        // TODO: Tracking methods
     }
 }
