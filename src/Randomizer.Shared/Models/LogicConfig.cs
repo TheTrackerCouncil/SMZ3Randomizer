e9d3507d45bc83574259cf0bc0870d24402685bf
fxbx <jjlarsen@gmail.com>
2022-09-02 11:48:05 -0400

    Update LogicConfig.cs
    Consistency in tooltips (periods on end of all) and second "gravity suit" edited to Waterway in Pink Brinstar

--- src/Randomizer.Shared/Models/LogicConfig.cs
+++ src/Randomizer.Shared/Models/LogicConfig.cs
@@ -1,11 +1,11 @@
-﻿using System.ComponentModel;
+using System.ComponentModel;
 
 namespace Randomizer.Shared
 {
     public class LogicConfig
     {
         [DisplayName("Prevent Screw Attack Soft Locks")]
-        [Description("You're not expected to use Screw Attack without the morph ball")]
+        [Description("You're not expected to use Screw Attack without the morph ball.")]
         [Category("Logic")]
         public bool PreventScrewAttackSoftLock { get; set; }
 
@@ -15,52 +15,52 @@ namespace Randomizer.Shared
         public bool PreventFivePowerBombSeed { get; set; }
 
         [DisplayName("Left Sand Pit Needs Spring Ball")]
-        [Description("You're expected to have the spring ball and hi jump boots to navigate the left sand pit in Maridia.")]
+        [Description("You're expected to have the spring ball and hi-jump boots to navigate the left sand pit in Maridia.")]
         [Category("Logic")]
         public bool LeftSandPitRequiresSpringBall { get; set; }
 
         [DisplayName("Launchpad Needs Ice Beam")]
-        [Description("You're expected to have the ice beam to freeze the boyons to run over to get speed for a shine spark in the old Tourian launchpad in central Crateria")]
+        [Description("You're expected to have the ice beam to freeze the boyons to run over to get speed for a shine spark in the old Tourian launchpad in central Crateria.")]
         [Category("Logic")]
         public bool LaunchPadRequiresIceBeam { get; set; }
 
         [DisplayName("Waterway Needs Gravity Suit")]
-        [Description("You're expected to have the gravity suit to be able to access the gravity suit")]
+        [Description("You're expected to have the gravity suit to be able to access the Waterway in Pink Brinstar.")]
         [Category("Logic")]
         public bool WaterwayNeedsGravitySuit { get; set; }
 
         [DisplayName("Easy East Crateria Sky Item")]
-        [Description("You're expected to have the space jump or speed booster to get the item in the sky in East Crateria after Wrecked Ship")]
+        [Description("You're expected to have the space jump or speed booster to get the item in the sky in East Crateria after Wrecked Ship.")]
         [Category("Logic")]
         public bool EasyEastCrateriaSkyItem { get; set; }
 
         [DisplayName("Fire Rod for Dark Rooms")]
-        [Description("You're expected to be able to use the fire rod to light torches for navigating Hyrule Castle escape, Eastern Palace Armos Knights, and select rooms in Palace of Darkness")]
+        [Description("You're expected to be able to use the fire rod to light torches for navigating Hyrule Castle escape, Eastern Palace Armos Knights, and select rooms in Palace of Darkness.")]
         [Category("Tricks")]
         public bool FireRodDarkRooms { get; set; }
 
         [DisplayName("Infinite Bomb Jump")]
-        [Description("You're expected to be able to use specially timed morph bombs to access high locations")]
+        [Description("You're expected to be able to use specially timed morph bombs to access high locations.")]
         [Category("Tricks")]
         public bool InfiniteBombJump { get; set; }
 
         [DisplayName("Parlor Speed Booster Break In")]
-        [Description("You're expected to be able to use the speed booster to burst through to the Terminator Room/Brinstar without morph/power bombs or screw attack")]
+        [Description("You're expected to be able to use the speed booster to burst through to the Terminator Room/Brinstar without morph/power bombs or screw attack.")]
         [Category("Tricks")]
         public bool ParlorSpeedBooster { get; set; }
 
         [DisplayName("Mockball")]
-        [Description("You're expected to be able to use to mockball to avoid having the speed booster at the entrance to Green Brinstar and Upper Norfair West")]
+        [Description("You're expected to be able to use to mockball to avoid having the speed booster at the entrance to Green Brinstar and Upper Norfair West.")]
         [Category("Tricks")]
         public bool MockBall { get; set; }
 
         [DisplayName("Sword Only Dark Rooms")]
-        [Description("Excluding Hyrule Castle Tower, you're expected to be able to pass through dark rooms without any light and using only your sword to navigate")]
+        [Description("Excluding Hyrule Castle Tower, you're expected to be able to pass through dark rooms without any light and using only your sword to navigate.")]
         [Category("Tricks")]
         public bool SwordOnlyDarkRooms { get; set; }
 
         [DisplayName("Light World South Fake Flippers")]
-        [Description("You're expected to be able to use fake flippers in the Light World South near Lake Hylia to access under the bridge and, if you have the moon pearl, the waterfall fairy chests")]
+        [Description("You're expected to be able to use fake flippers in the Light World South near Lake Hylia to access under the bridge and, if you have the moon pearl, the waterfall fairy chests.")]
         [Category("Tricks")]
         public bool LightWorldSouthFakeFlippers { get; set; }
 
