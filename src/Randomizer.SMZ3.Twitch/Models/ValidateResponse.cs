9c09051b72efc8b49801042636a58f9e990f1733
Laura Verdoes <1766841+Vivelin@users.noreply.github.com>
2022-09-04 12:15:42 +0200

    Validate Twitch OAuth tokens on startup
    Also hides the token in the Options screen.
    
    Resolves #114 and resolves #82

---
+++ src/Randomizer.SMZ3.Twitch/Models/ValidateResponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+using Randomizer.SMZ3.ChatIntegration.Models;
+
+namespace Randomizer.SMZ3.Twitch.Models
+{
+    public class ValidateResponse : TwitchAPIResponse
+    {
+        [JsonPropertyName("client_id")]
+        public string? ClientId { get; set; }
+
+        [JsonPropertyName("login")]
+        public string? Login { get; set; }
+
+        [JsonPropertyName("scopes")]
+        public IReadOnlyCollection<string>? Scopes { get; set; }
+
+        [JsonPropertyName("user_id")]
+        public string? UserId { get; set; }
+
+        [JsonPropertyName("expires_in")]
+        public int? ExpiresIn { get; set; }
+
+        [JsonPropertyName("status")]
+        public int? Status { get; set; }
+
+        [JsonPropertyName("messages")]
+        public string? Message { get; set; }
+
+        [JsonIgnore]
+        public new bool IsSuccessful { get; set; }
+    }
+}
--- src/Randomizer.SMZ3.Twitch/TwitchAuthenticationService.cs
+++ src/Randomizer.SMZ3.Twitch/TwitchAuthenticationService.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Extensions.Logging;
+
 using Randomizer.SMZ3.ChatIntegration;
 using Randomizer.SMZ3.ChatIntegration.Models;
 using Randomizer.SMZ3.Twitch.Models;
@@ -10,11 +15,18 @@ namespace Randomizer.SMZ3.Twitch
 {
     public class TwitchAuthenticationService : OAuthChatAuthenticationService
     {
-        private readonly IChatApi _twitchChatAPI;
+        private const string ValidateTokenEndpoint = "https://id.twitch.tv/oauth2/validate";
 
-        public TwitchAuthenticationService(IChatApi twitchChatAPI)
+        private static readonly HttpClient s_httpClient = new();
+
+        private readonly IChatApi _twitchChatAPI;
+        private readonly ILogger<TwitchAuthenticationService> _logger;
+
+        public TwitchAuthenticationService(IChatApi twitchChatAPI,
+            ILogger<TwitchAuthenticationService> logger)
         {
             _twitchChatAPI = twitchChatAPI;
+            _logger = logger;
         }
 
         public override Uri GetOAuthUrl(Uri redirectUri)
@@ -24,6 +36,44 @@ namespace Randomizer.SMZ3.Twitch
                 $"&redirect_uri={Uri.EscapeDataString(redirectUri.ToString())}" +
                 "&response_type=token" +
                 $"&scope={Uri.EscapeDataString("chat:read chat:edit channel:moderate channel:read:polls channel:manage:polls channel:read:predictions channel:manage:predictions")}");
+        }
+
+        public override async Task<bool> ValidateTokenAsync(string accessToken, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, new Uri(ValidateTokenEndpoint));
+                request.Headers.Authorization = new("OAuth", accessToken);
+
+                var response = await s_httpClient.SendAsync(request, cancellationToken);
+                if (response.IsSuccessStatusCode)
+                {
+                    using var responseStream = await response.Content.ReadAsStreamAsync(cancellationToken);
+                    var validateResponse = await JsonSerializer.DeserializeAsync<ValidateResponse>(responseStream, cancellationToken: cancellationToken);
+                    if (validateResponse?.ExpiresIn > 0)
+                        return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unexpected error occurred while checking Twitch OAuth token validity.");
+            }
+
+            return false;
+        }
+
+        public async Task<bool> RevokeTokenAsync(string accessToken, CancellationToken cancellationToken)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unexpected error occurred while revoking Twitch OAuth token.");
+            }
+
+            return false;
         }
 
         public override async Task<AuthenticatedUserData?> GetAuthenticatedUserDataAsync(string accessToken, CancellationToken cancellationToken)
