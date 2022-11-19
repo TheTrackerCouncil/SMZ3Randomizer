966e96b6b3a97cd474e508e168b014abdfe8cbdd
MattEqualsCoder <63823784+MattEqualsCoder@users.noreply.github.com>
2022-11-19 11:52:31 -0500

    Whoops

--- src/Randomizer.App/RomGenerator.cs
+++ src/Randomizer.App/RomGenerator.cs
@@ -162,8 +162,8 @@ namespace Randomizer.App
 
             // For BizHawk shuffler support, the file name is checked when running the BizHawk Auto Tracking Lua script
             // If the fom file name is changed, make sure to update the BizHawk emulator.lua script and the LuaConnector
-            var romFileName = $"SMZ3_Cas_{fileSuffix}.sfc";
             var fileSuffix = $"{DateTimeOffset.Now:yyyyMMdd-HHmmss}_{safeSeed}";
+            var romFileName = $"SMZ3_Cas_{fileSuffix}.sfc";
             var romPath = Path.Combine(folderPath, romFileName);
             EnableMsu1Support(options, bytes, romPath, out var msuError);
             Rom.UpdateChecksum(bytes);
