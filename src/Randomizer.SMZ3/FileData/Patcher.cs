803ea8d310ae9284eb0f3e90f06a9927ca6881ec
MattEqualsCoder <mattequalscoder@gmail.com>
2023-01-08 23:35:41 -0500

    Fixing dungeon music for non-MSU users

--- src/Randomizer.SMZ3/FileData/Patcher.cs
+++ src/Randomizer.SMZ3/FileData/Patcher.cs
@@ -99,6 +99,7 @@ namespace Randomizer.SMZ3.FileData
         {
             WriteMedallions();
             WriteRewards();
+            WriteDungeonMusic(config.Keysanity);
             WriteRngBlock();
             WriteDiggingGameRng();
             WritePrizeShuffle();
@@ -912,6 +913,42 @@ namespace Randomizer.SMZ3.FileData
                     ? $"{hintText} belonging to you"
                     : $"{hintText} belonging to {item.World.Player}";
             }
+        }
+
+        private void WriteDungeonMusic(bool keysanity)
+        {
+            if (keysanity) return;
+            var regions = _myWorld.Regions.OfType<IHasReward>().Where(x => x.RewardType != RewardType.Agahnim);
+            var music = regions.Select(x => (byte)(x.RewardType switch {
+                RewardType.PendantBlue => 0x11,
+                RewardType.PendantGreen => 0x11,
+                RewardType.PendantRed => 0x11,
+                _ => 0x16
+            }));
+
+            _patches.AddRange(MusicPatches(regions, music));
+        }
+
+        private IEnumerable<(int, byte[])> MusicPatches(IEnumerable<IHasReward> regions, IEnumerable<byte> music) {
+            var addresses = regions.Select(MusicAddresses);
+            var associations = addresses.Zip(music, (a, b) => (a, b));
+            return associations.SelectMany(x => x.a.Select(i => (Snes(i), new byte[] { x.b })));
+        }
+
+        private int[] MusicAddresses(IHasReward region) {
+            return region switch {
+                EasternPalace _ => new[] { 0x2D59A },
+                DesertPalace _ => new[] { 0x2D59B, 0x2D59C, 0x2D59D, 0x2D59E },
+                TowerOfHera _ => new[] { 0x2D5C5, 0x2907A, 0x28B8C },
+                PalaceOfDarkness _ => new[] { 0x2D5B8 },
+                SwampPalace _ => new[] { 0x2D5B7 },
+                SkullWoods _ => new[] { 0x2D5BA, 0x2D5BB, 0x2D5BC, 0x2D5BD, 0x2D608, 0x2D609, 0x2D60A, 0x2D60B },
+                ThievesTown _ => new[] { 0x2D5C6 },
+                IcePalace _ => new[] { 0x2D5BF },
+                MiseryMire _ => new[] { 0x2D5B9 },
+                TurtleRock _ => new[] { 0x2D5C7, 0x2D5A7, 0x2D5AA, 0x2D5AB },
+                var x => throw new InvalidOperationException($"Region {x} should not be a dungeon music region"),
+            };
         }
     }
 }
