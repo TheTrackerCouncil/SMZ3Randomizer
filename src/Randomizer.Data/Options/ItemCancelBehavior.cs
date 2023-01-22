5288a072f94071dfd405ce7fc00d5d33f76dca30
MattEqualsCoder <mattequalscoder@gmail.com>
2023-01-22 10:47:22 -0500

    PR updates

--- src/Randomizer.Data/Options/ItemCancelBehavior.cs
+++ src/Randomizer.Data/Options/ItemCancelBehavior.cs
@@ -1,11 +1,16 @@
-﻿namespace Randomizer.Data.Options;
+﻿using System.ComponentModel;
+
+namespace Randomizer.Data.Options;
 
 /// <summary>
 /// Enum for the different behaviors for pressing the item cancel button in SM
 /// </summary>
 public enum ItemCancelBehavior
 {
+    [Description("Press to deselect item (Vanilla)")]
     Vanilla,
+    [Description("Hold and press shoot to fire supers/power bombs")]
     Hold,
+    [Description("Press to toggle supers/pbs on or off")]
     Toggle
 }
