bc1c5c420ab11466f226d030e76225c2603ce6fb
MattEqualsCoder <mattequalscoder@gmail.com>
2023-06-21 09:40:23 -0400

    Adding DeathLink Migration

--- src/Randomizer.Multiplayer.Server/Migrations/20230621133443_DeathLink.Designer.cs
+++ src/Randomizer.Multiplayer.Server/Migrations/20230621133443_DeathLink.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Randomizer.Multiplayer.Server;
 
@@ -10,9 +11,10 @@ using Randomizer.Multiplayer.Server;
 namespace Randomizer.Multiplayer.Server.Migrations
 {
     [DbContext(typeof(MultiplayerDbContext))]
-    partial class MultiplayerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230621133443_DeathLink")]
+    partial class DeathLink
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.11");
